af9c1ece8ab501c3bcf39cd27f031750025c01fb
Alan <Alan.s11@aluno.ifsc.edu.br>
2026-07-28 20:11:10 -0300

    Revisão de Java

---
+++ aulas/2026-07-28/OlaMundo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OlaMundo {
+    public static void main(String args[]){
+        System.out.println("Ola mundo");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com a sua idade");
+        int idade = sc.nextInt();
+
+        System.out.println("Entre com o seu sexo");
+        char sexo =sc.next().charAt(0);
+
+        if(sexo.equals("m")){
+     
+        }
+
+
+
+
+
+    }
+}
